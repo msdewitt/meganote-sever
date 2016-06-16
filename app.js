ceb9106b3218f568f86d8d71d1554fc2e4b6d3a6
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-16 10:57:46 -0400

    Send a hardcoded JSON response.

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ var express = require('express');//require imports a module into your project
 
 var app = express(); // run the module
 
-app.get('/', function(req,res){
-  res.send('I love Express')
-})//responding to a get request from a client then do this
+app.get('/', function(req, res) {
+  res.json([
+    {
+      title: 'Hardcoded note',
+      body_html: 'My cool note.'
+    },
+    {
+      title: 'Also hardcoded',
+      body_html: 'Node is fun.'
+    }
+  ]);
+});//responding to a get request from a client then do this
 
 
 app.listen(3030, function(){
